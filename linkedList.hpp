4b73d62c95a419f84661a1e5c36b259e7e34070a
Ricardo Joel Gonzalez Velasco <70970994+ricardgo403@users.noreply.github.com>
2020-11-04 22:40:02 -0600

    LinkedList node and initial attributes.

--- linkedList.hpp
+++ linkedList.hpp
@@ -1,12 +1,25 @@
-// linkedList.hpp 
-#ifndef _LINKEDLIST_HPP_
-#define _LINKEDLIST_HPP_
-  
-template<class T>
-class LinkedList   
-{
-public:
-    LinkedList();
-private:
+// linkedList.hpp
+#ifndef LINKEDLIST_HPP
+#define LINKEDLIST_HPP
+
+template <class T>
+class LinkedList
+{   
+    private:
+        #include "node.hpp"
+        size_t listSize;
+        Node *listFront;
+        Node *listBack;
+
+    public:
+        LinkedList<T>() {
+            listSize = 0;
+            listFront = nullptr;
+            listBack = nullptr;
+        };
+
+        ~LinkedList<T>();
+        bool empty() const;
+        size_t size();
 };
 #endif
